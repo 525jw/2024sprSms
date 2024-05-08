d1db7e1077fb11ed9cb2e7b7279fd15c77791762
안재원 <101179346+525jw@users.noreply.github.com>
2024-05-09 01:41:08 +0900

    20240509 ax9.2 not sub

--- AX2024/ax.9/2/2.cpp
+++ AX2024/ax.9/2/2.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <map>
 using namespace std;
 typedef struct node* node_ptr;
 typedef struct node{
@@ -15,6 +16,7 @@ struct compare{
         return p->frequency > q->frequency;
     }
 };
+typedef priority_queue<node_ptr,vector<node_ptr>,compare> PriorityQueue;
 node_ptr create_node(char newsymbol,int newfrequency){
     node_ptr newnode=(node_ptr)malloc(sizeof(node_t));
     newnode->symbol=newsymbol;
@@ -23,7 +25,6 @@ node_ptr create_node(char newsymbol,int newfrequency){
     newnode->right=NULL;
     return newnode;
 }
-typedef priority_queue<node_ptr,vector<node_ptr>,compare> PriorityQueue;
 void huffman(int n,PriorityQueue& PQ){
     for(int i=1;i<=n-1;i++){
         // pop the highest two values
@@ -35,7 +36,29 @@ void huffman(int n,PriorityQueue& PQ){
         PQ.push(r);
     }
 }
-
+//- make an encoder for each char using the root node of the Huffmantree
+void make_encoder(node_ptr node, string code, map<char,string> &encoder){
+    if(node->symbol !='+'){
+        encoder[node->symbol]=code;
+    }else{
+        make_encoder(node->left,code+"0",encoder);
+        make_encoder(node->right,code+"1",encoder);
+    }
+}
+//- 
+void decode(node_ptr root, node_ptr node, string s,int i){
+    if(i<=s.length()){
+        if(node->symbol!='+'){
+            cout<<node->symbol;
+            decode(root,root,s,i);
+        }else{
+            if(s[i]=='0')
+                decode(root,node->left,s,i+1);
+            else
+                decode(root,node->right,s,i+1);
+        }
+    }
+}
 int main(){
     int n; cin>>n;
     vector<char> symb(n+1);
@@ -44,23 +67,29 @@ int main(){
         cin>>symb[i];
     for(int i=0;i<n;i++)
         cin>>frq[i];
-    int t1; cin>>t1;
 
+    PriorityQueue PQ;
     for(int i=0;i<n;i++)
-        PriorityQueue.push(create_node(symb[i],frq[i]));
+        PQ.push(create_node(symb[i],frq[i]));
+    huffman(n,PQ);
 
+    map<char,string> encoder;
+    make_encoder(PQ.top(),"",encoder);
+    
+    //encode the given text for t1 times using the map 'encoder'
+    int t1; cin>>t1;
+    string inp,encoded;
+    for(int i=0;i<t1;i++){
+        inp.clear(); encoded.clear();
+        cin>>inp;
+        for(int j=0;j<inp.size();j++)
+            encoded+=encoder[inp[j]];
+        cout<<encoded<<endl;
+    }
 
-
-
-
-
-    vector<string> str_to_encode(n+1);
-    for(int i=0;i<t1;i++)
-        cin>>str_to_encode[i];
     int t2; cin>>t2;
-    vector<string> str_to_decode(n+1);
-    for(int i=0;i<t2;i++)
-        cin>>str_to_decode[i];
+
+
 
 
 
--- TEST/test.cpp
+++ TEST/test.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
-int main(){
-    for(int i=0;i<32;i++){
-        for(int j=0;j<32;j++){
-            cout<<1<<' ';
+
+int cache[101][100001];
+
+void solve(vector<pair<int, int>> vc, int n, int k) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j <= k; j++) {
+            if(vc[i].first <= j && vc[i].second + cache[i-1][j-vc[i].first] > cache[i-1][j]) 
+                cache[i][j] = vc[i].second + cache[i-1][j-vc[i].first];
         }
-        cout<<endl;
     }
+}
+
+int main() {
+    int n, k, w, v;
+    vector<pair<int, int>> vc;
+    cin >> n >> k;
+    vc.resize(n+1);
+    for(int i = 1; i <= n; i++) {
+        cin >> w >> v;
+        vc[i] = make_pair(w, v);
+    }
+    solve(vc, n, k);
+    cout << cache[n][k];
 }
